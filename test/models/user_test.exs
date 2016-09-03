3d8557f32747059cc5ff806d8447bf19bc8bafe9
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-09-03 11:47:38 -0300

    Change user tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,8 +3,8 @@ defmodule Featureyard.UserTest do
 
   alias Featureyard.User
 
-  @valid_attrs %{email: "some content", encrypted_password: "some content"}
-  @invalid_attrs %{}
+  @valid_attrs %{email: "evangelistajoaop@gmail.com", encrypted_password: "123456789"}
+  @invalid_attrs %{email: "", encrypted_password: "1234566789"}
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
