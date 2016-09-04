b6a0da2d092bcf21566d73b612d7a44fde8b8cf2
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-09-04 14:58:12 -0300

    Add a check for token header key using plug

--- web/controllers/feature_api_controller.ex
+++ web/controllers/feature_api_controller.ex
@@ -6,8 +6,12 @@ defmodule Featureyard.Api.FeatureController do
   alias Featureyard.Repo
   alias Featureyard.Client
 
+  plug :ensure_api_key
+
+  @token_header "x-token"
+
   def index(conn, _params) do
-    case conn |> get_req_header("x-token") |> Enum.at(0, "") |> Base.decode64 do
+    case conn |> get_req_header(@token_header) |> Enum.at(0, "") |> Base.decode64 do
       {:ok, key} ->
         client = Repo.get_by!(Client, key: key)
         |> Repo.preload(:features)
@@ -16,5 +20,15 @@ defmodule Featureyard.Api.FeatureController do
       {:error} ->
         render(conn, "error.json")
     end
+  end
+
+
+  def ensure_api_key(conn, _options) do
+     header = conn |> get_req_header(@token_header)
+     if Enum.at(header, 0) != nil do
+       conn
+     else
+       conn |> send_resp(401, "")
+     end
   end
 end
